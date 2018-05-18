98fb22eb80a6498edf29c0c4a16c1497a1acf00b
obieler <olivier.bieler@epfl.ch>
2018-05-18 13:48:56 +0200

    copyright, /usr/bin/env bash, nscd restart only if exist

--- scripts/flush-dns.sh
+++ scripts/flush-dns.sh
@@ -1,15 +1,17 @@
-
-
-#!/bin/bash
-
-# NB: First install nscd with sudo apt-get install nscd
+#!/usr/bin/env bash
+#
+# (c) ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, VPSI, 2017-2018.
+# See the LICENSE file for more details.
+#
 
 # run this command to flush dns cache:
 sudo /etc/init.d/dns-clean restart
 # or use:
 sudo /etc/init.d/networking force-reload
-# Flush nscd dns cache:
-sudo /etc/init.d/nscd restart
+# Flush nscd dns cache if exist
+if [ -n "$(command -v nscd)" ]; then
+    sudo /etc/init.d/nscd restart
+fi
 # If you wanted to refresh your settings you could disable and then run
 sudo service network-manager restart
 
